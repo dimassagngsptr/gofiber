807828ae303708fa29606e5b79bfc179a7f1deb7
dimassagngsptr <dimasageng58@gmail.com>
2024-05-10 22:34:55 +0700

    fix: change database from local db into mentor database

--- src/helper/migration.go
+++ src/helper/migration.go
@@ -1,14 +1,12 @@
 package helper
 
 import (
-	_"gofiber/src/configs"
-	_"gofiber/src/models"
+	"gofiber/src/configs"
+	"gofiber/src/models"
 )
 
 func Migration() {
-	// configs.DB.AutoMigrate(&models.Product{},&models.Category{})
-	// configs.DB.AutoMigrate(&models.Address{}, &models.User{})
-	// configs.DB.AutoMigrate(&models.User{})
-	// configs.DB.AutoMigrate(&models.UserAddress{})
+	configs.DB.AutoMigrate(&models.Product{},&models.Category{},&models.Address{}, &models.User{})
+
 
 }
