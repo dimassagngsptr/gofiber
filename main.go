7c4db52c604846f6648b20b27f6f011ec9e164c6
dimassagngsptr <dimasageng58@gmail.com>
2024-06-12 09:47:22 +0700

    fix

--- main.go
+++ main.go
@@ -5,22 +5,24 @@ import (
 	"gofiber/src/helpers"
 	"gofiber/src/routes"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
-	_"github.com/gofiber/fiber/v2/middleware/cors"
+	_ "github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(err)
+	if _, err := os.Stat(".env"); err == nil {
+		if err := godotenv.Load(); err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
-	app:=fiber.New()
+	app := fiber.New()
 
 	app.Use(helmet.New())
-	app.Static("/public","./src/public")
+	// app.Static("/public", "./src/public")
 	// app.Use(cors.New(cors.Config{
 	// 	AllowOrigins: "*",
 	// 	AllowMethods: "GET POST PUT PATCH DELETE",
@@ -31,7 +33,6 @@ func main() {
 	configs.InitDB()
 	helpers.Migration()
 	routes.Router(app)
-	
 
 	app.Listen(":3000")
 }
