f9e7d79c225f8c8759325f447025028920e2b715
dimassagngsptr <dimasageng58@gmail.com>
2024-06-12 09:52:45 +0700

    fix

--- main.go
+++ main.go
@@ -12,6 +12,14 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/helmet"
 	"github.com/joho/godotenv"
 )
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000"
+	}
+	return "0.0.0.0:" + port
+
+}
 
 func main() {
 	if _, err := os.Stat(".env"); err == nil {
@@ -34,5 +42,5 @@ func main() {
 	helpers.Migration()
 	routes.Router(app)
 
-	app.Listen(":3000")
+	app.Listen(getPort())
 }
